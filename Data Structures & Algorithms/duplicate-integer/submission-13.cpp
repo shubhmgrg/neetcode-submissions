1a51b80fc21ae11226ab1b3a0d86ed7b3470d767
shubhmgrg <144749064+shubhmgrg@users.noreply.github.com>
2026-08-24 13:01:15 -0400

    Add: duplicate-integer - submission-13

---
+++ Data Structures & Algorithms/duplicate-integer/submission-13.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        set<int> uniques = {};
+        for(int num : nums){
+            if(uniques.contains(num)){
+                return true;
+            }
+            uniques.insert(num);
+        }
+        return false;
+    }
+};
